afdd8c6eaed9a53ec7cb634037f69adfdd0b67f8
ThembiShabalala <thembi.shabalala@umuzi.org>
2019-06-14 14:05:08 +0200

    first draft

--- passwordchecker.js
+++ passwordchecker.js
@@ -6,27 +6,27 @@ function password_is_valid(password){
     }
    
     return validPassword.test(password);
-   }
+}
 
 
-   function password_is_ok(password){
-        let pass1 = /[A-Za-z0-9]{9,}/;
-       let pass2 = /(?=.*[A-Z])[A-Z]+/;
-       let pass3 = /(?=.*[a-z])[a-z]+/;
-       let pass4 = /(?=.*[0-9])[0-9]+/;
-       if(password !== null && pass1.test(password) == true && pass2.test(password) == true){
+function password_is_ok(password){
+    let pass1 = /[A-Za-z0-9]{9,}/;
+    let pass2 = /(?=.*[A-Z])[A-Z]+/;
+    let pass3 = /(?=.*[a-z])[a-z]+/;
+    let pass4 = /(?=.*[0-9])[0-9]+/;
+    if(password !== null && pass1.test(password) == true && pass2.test(password) == true){
            return true;
-       } 
-       if(password !== null && pass1.test(password)&& pass1.test(password) == true && pass3.test(password) == true){
+} 
+    if(password !== null && pass1.test(password)&& pass1.test(password) == true && pass3.test(password) == true){
         return true;
-   }
+ }
    if(password !== null && pass1.test(password) == true && pass4.test(password) == true){
     return true;
-   }
+}
 else {
     return false;
 }
-   }
-   console.log(password_is_valid("Thembi"));
+}
+
 
 
